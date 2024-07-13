0237b33eb9c9c36bee45546bd94425aa4a49acc6
xavi <xavigrafik@gmail.com>
2024-07-13 13:49:27 +0200

    16-chanchito

---
+++ projects/16-chanchito/react-app/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Card from './components/Card';
+import { CardBody } from "./components/Card";
+
+function App() {
+    return (
+        <>
+            <Card>
+                <CardBody title="Hola" text="Text"/>
+            </Card>
+        </>
+    );
+}
+
+export default App;
+
+
